82261304721efd62371df584443ce181491df6b0
xuges <xuges@qq.com>
2025-06-06 17:55:02 +0800

    Add Linux implement.

---
+++ daemon_other.go
@@ -0,0 +1,24 @@
+//go:build linux
+
+package godaemon
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func startDaemonProcess(name string, args []string, attr *ProcAttr) error {
+	cmd := exec.Command(name, args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid:   true,
+		Pgid:      1,
+		Pdeathsig: syscall.Signal(0),
+	}
+
+	if attr != nil {
+		cmd.Env = attr.Env
+		cmd.Dir = attr.Dir
+	}
+
+	return cmd.Start()
+}
